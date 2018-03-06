a6a6a7333c3570ab288dd865c644fc256f3cbfef
Rob <rob@layermade.com>
2018-03-06 19:25:03 +0000

    Add a location field which is required

--- marshmallow-test/app.py
+++ marshmallow-test/app.py
@@ -2,19 +2,21 @@ from marshmallow import Schema, fields, pprint, post_load
 
 
 class Person(object):
-    def __init__(self, name, age, email):
+    def __init__(self, name, age, email, location):
         self.name = name
         self.age = age
         self.email = email
+        self.location = location
 
     def __repr__(self):
-        return '{} is {} years old. Your email is {}'.format(self.name, self.age, self.email)
+        return '{} is {} years old. Your email is {}'.format(self.name, self.age, self.email, self.location)
 
 
 class PersonSchema(Schema):
     name = fields.String()
     age = fields.Integer()
     email = fields.Email()
+    location = fields.String(required=True)
 
     # Auto load the data into the
     # Person object. Unpack and use the
@@ -29,6 +31,7 @@ input_dict = {}
 input_dict['name'] = input('What is your name? ')
 input_dict['age'] = input('How old are you? ')
 input_dict['email'] = input('What\'s your email? ')
+input_dict['location'] = input('What\'s your location? ')
 
 schema = PersonSchema()
 result = schema.load(input_dict)
