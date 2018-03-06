01ce685c82e13e70654f0961c4e2fc2ceeedbdbf
Rob <rob@layermade.com>
2018-03-06 18:47:44 +0000

    First create an `input_dict` using `input()`, then pass it through to `Person()` class.

---
+++ marshmallow-test/app.py
@@ -0,0 +1,25 @@
+from marshmallow import Schema, fields
+
+
+class Person(object):
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    def __repr__(self):
+        return '{} is {} years old'.format(self.name, self.age)
+
+
+class PersonSchema(Schema):
+    name = fields.String()
+    age = fields.Integer()
+
+
+input_dict = {}
+
+input_dict['name'] = input('What is your name? ')
+input_dict['age'] = input('How old are you? ')
+
+person = Person(name=input_dict['name'], age=input_dict['age'])
+
+print(person)
